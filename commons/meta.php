c3627f27ff437868e34d7f399b9ea8537667da7e
Diki Siswanto <dickyachmady@gmail.com>
2019-03-29 08:47:11 +0800

    Perbaiki meta web dan perbaikan kode captcha

--- commons/meta.php
+++ commons/meta.php
@@ -10,27 +10,27 @@
 <meta name='designer' content='Diki Siswanto' />
 <meta name='theme:designer' content='Diki Siswanto' />
 <meta name='theme:version' content='1.0' />
-<meta name='keywords' content="sid, sistem informasi desa, web, blog, informasi, website, cosmos, desa, kecamatan, kabupaten, indonesia, kampung, <?php echo $desa['nama_desa']; ?>, <?php echo $desa['nama_kecamatan']; ?>, <?php echo $desa['nama_kabupaten']; ?>" />
-<meta property="og:site_name" content="<?php echo $desa_title ?>"/>
+<meta name='keywords' content="sid, sistem informasi desa, web, blog, informasi, website, cosmos, desa, kecamatan, kabupaten, indonesia, kampung, <?= $desa['nama_desa']; ?>, <?= $desa['nama_kecamatan']; ?>, <?= $desa['nama_kabupaten']; ?>" />
+<meta property="og:site_name" content="<?= $desa_title ?>"/>
 <meta property="og:type" content="article"/>
 
 <?php if(isset($single_artikel)): ?>
-	<title><?php echo $single_artikel["judul"] . " - $desa_title" ?></title>
-	<meta name='description' content="<?php echo str_replace('"', "'", substr(strip_tags($single_artikel['isi']), 0, 400)); ?>" />
-	<meta property="og:title" content="<?php echo $single_artikel["judul"];?>"/>
+	<title><?= $single_artikel["judul"] . " - $desa_title" ?></title>
+	<meta name='description' content="<?= str_replace('"', "'", substr(strip_tags($single_artikel['isi']), 0, 400)); ?>" />
+	<meta property="og:title" content="<?= $single_artikel["judul"];?>"/>
 	<?php if (trim($single_artikel['gambar'])!=''): ?>
-	<meta property="og:image" content="<?php echo base_url()?><?php echo LOKASI_FOTO_ARTIKEL?>sedang_<?php echo $single_artikel['gambar'];?>"/>
+	<meta property="og:image" content="<?= base_url()?><?= LOKASI_FOTO_ARTIKEL?>sedang_<?= $single_artikel['gambar'];?>"/>
 	<?php endif; ?>
-	<meta property='og:description' content="<?php echo str_replace('"', "'", substr(strip_tags($single_artikel['isi']), 0, 400)); ?>" />
+	<meta property='og:description' content="<?= str_replace('"', "'", substr(strip_tags($single_artikel['isi']), 0, 400)); ?>" />
 <?php else: ?>
 	<title><?php $tmp = ltrim(get_dynamic_title_page_from_path(), ' -'); echo (trim($tmp)=='') ? $desa_title : "$tmp - $desa_title"; ?></title>
-	<meta name='description' content="<?php echo $this->setting->website_title . ' ' . $desa_title; ?>" />
-	<meta property="og:title" content="<?php echo $desa_title;?>"/>
-	<meta property='og:description' content="<?php echo $this->setting->website_title . ' ' . $desa_title; ?>" />
+	<meta name='description' content="<?= $this->setting->website_title . ' ' . $desa_title; ?>" />
+	<meta property="og:title" content="<?= $desa_title;?>"/>
+	<meta property='og:description' content="<?= $this->setting->website_title . ' ' . $desa_title; ?>" />
 <?php endif; ?>
-<meta property='og:url' content="<?php echo current_url(); ?>" />
+<meta property='og:url' content="<?= current_url(); ?>" />
 <?php if(is_file(LOKASI_LOGO_DESA . "favicon.ico")): ?>
-<link rel="shortcut icon" href="<?php echo base_url() . LOKASI_LOGO_DESA?>favicon.ico" />
+<link rel="shortcut icon" href="<?= base_url() . LOKASI_LOGO_DESA?>favicon.ico" />
 <?php else: ?>
-<link rel="shortcut icon" href="<?php echo base_url('favicon.ico')?>" />
+<link rel="shortcut icon" href="<?= base_url('favicon.ico')?>" />
 <?php endif; ?>
