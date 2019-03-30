38e618ee699618019ef82362e0fff80a01aa4d8c
Diki Siswanto <dickyachmady@gmail.com>
2019-03-30 21:32:42 +0800

    Perbaikan #2

--- commons/source_css.php
+++ commons/source_css.php
@@ -1,20 +1,20 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <link href="https://fonts.googleapis.com/css?family=Montserrat:300,600,700" rel="stylesheet">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/bootstrap/css/bootstrap.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/font-awesome/css/all.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/font-awesome/css/fontawesome.min.css")?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/ace-menu/css/ace-responsive-menu.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/owl-carousel/css/owl.carousel.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/owl-carousel/css/owl.theme.default.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/fancybox/css/jquery.fancybox.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/jssocial/css/jssocials.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/vendor/jssocial/css/jssocials-theme-flat.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/bootstrap/css/bootstrap.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/font-awesome/css/all.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/font-awesome/css/fontawesome.min.css")?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/ace-menu/css/ace-responsive-menu.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/owl-carousel/css/owl.carousel.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/owl-carousel/css/owl.theme.default.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/fancybox/css/jquery.fancybox.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/jssocial/css/jssocials.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/vendor/jssocial/css/jssocials-theme-flat.css") ?>">
 <link rel="stylesheet" href="<?= base_url("assets/css/leaflet.css")?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/assets/css/animate.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/assets/css/loader.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/assets/css/comments.min.css") ?>">
-<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/assets/css/style.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/assets/css/animate.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/assets/css/loader.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/assets/css/comments.min.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->$theme_folder/$this->theme/assets/css/style.min.css") ?>">
 <?php if (is_file("desa/css/$this->theme/desa-web.css")): ?>
 	 <!-- Extra CSS untuk tema cosmos. Timpa aturan css di berkas ini. -->
 	<link rel="stylesheet" href="<?php echo base_url("desa/css/$this->theme/desa-web.css"); ?>">
--- commons/source_js.php
+++ commons/source_js.php
@@ -1,13 +1,13 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<script src="<?= base_url("$this->$theme_folder/vendor/jquery/jquery.min.js") ?>"></script>
-<script src="<?= base_url("$this->$theme_folder/vendor/bootstrap/js/bootstrap.min.js") ?>"></script>
-<script src="<?= base_url("$this->$theme_folder/vendor/jquery/jquery.marquee.min.js") ?>"></script>
-<script src="<?= base_url("$this->$theme_folder/vendor/owl-carousel/js/owl.carousel.min.js") ?>"></script>
-<script src="<?= base_url("$this->$theme_folder/vendor/ace-menu/js/ace-responsive-menu.min.js") ?>"></script>
-<script src="<?= base_url("$this->$theme_folder/vendor/fancybox/js/jquery.fancybox.min.js") ?>"></script>
-<script src="<?= base_url("$this->$theme_folder/vendor/jssocial/js/jssocials.min.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/vendor/jquery/jquery.min.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/vendor/bootstrap/js/bootstrap.min.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/vendor/jquery/jquery.marquee.min.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/vendor/owl-carousel/js/owl.carousel.min.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/vendor/ace-menu/js/ace-responsive-menu.min.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/vendor/fancybox/js/jquery.fancybox.min.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/vendor/jssocial/js/jssocials.min.js") ?>"></script>
 <script src="<?= base_url("assets/js/jquery.cycle2.min.js") ?>"></script>
 <script src="<?= base_url("assets/js/jquery.cycle2.carousel.js") ?>"></script>
 <script src="<?= base_url("assets/js/leaflet.js")?>"></script>
-<script src="<?= base_url("$this->$theme_folder/assets/js/script.js") ?>"></script>
+<script src="<?= base_url("$this->$theme_folder/$this->theme/assets/js/script.js") ?>"></script>
