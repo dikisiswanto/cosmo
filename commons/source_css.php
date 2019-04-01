405d52e14ed3d45224463d520dcf23ce99da24ce
Diki Siswanto <dickyachmady@gmail.com>
2019-04-02 07:30:31 +0800

    Muat sumber bootstrap yang sudah dikompresi

--- commons/source_css.php
+++ commons/source_css.php
@@ -1,7 +1,7 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <link href="https://fonts.googleapis.com/css?family=Montserrat:300,600,700" rel="stylesheet">
-<link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/vendor/bootstrap/css/bootstrap.css") ?>">
+<link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/vendor/bootstrap/css/bootstrap.min.css") ?>">
 <link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/vendor/font-awesome/css/all.min.css") ?>">
 <link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/vendor/font-awesome/css/fontawesome.min.css")?>">
 <link rel="stylesheet" href="<?= base_url("$this->theme_folder/$this->theme/vendor/ace-menu/css/ace-responsive-menu.min.css") ?>">
