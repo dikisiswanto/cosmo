7089b0b9f2062dbe4b8b864eea2059c0ad844bff
Diki Siswanto <dickyachmady@gmail.com>
2019-04-06 15:01:12 +0800

    Hilangkan fitur pencarian di widget

--- partials/widget.php
+++ partials/widget.php
@@ -1,17 +1,5 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<div class="box">
-	<form action="<?= site_url('first');?>" method="get" id="search">
-		<div class="input-group">
-			<input type="text" class="form-control" placeholder="Cari..." name="cari">
-			<span class="input-group-append">
-				<button class="btn btn-transparent" type="submit">
-					<i class="fa fa-search"></i>
-				</button>
-			</span>
-		</div>
-	</form>
-</div>
 <?php if($w_cos) : ?>
 	<?php foreach($w_cos as $data) : ?>
 		<?php if($data['jenis_widget'] == 1) : ?>
