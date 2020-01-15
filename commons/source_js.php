34b6edf18b4e93c3060787a666b8c9ade248b455
balongbesuk <35319600+balongbesuk@users.noreply.github.com>
2020-01-15 22:52:37 +0700

    Update source_js.php
    Perbaikan Slider Perangkat Desa

--- commons/source_js.php
+++ commons/source_js.php
@@ -1,7 +1,8 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/vendor.min.js") ?>"></script>
-<script src="<?= base_url("assets/js/jquery.cycle2.min.js") ?>"></script>
-<script src="<?= base_url("assets/js/jquery.cycle2.carousel.js") ?>"></script>
 <script src="<?= base_url("assets/js/leaflet.js")?>"></script>
 <script defer src="<?= base_url("$this->theme_folder/$this->theme/assets/js/script.min.js") ?>"></script>
+<!-- Untuk carousel, slider, teks_berjalan dan widget aparatur_desa -->
+<script src="<?php echo base_url()?>assets/front/js/jquery.cycle2.min.js"></script>
+<script src="<?php echo base_url()?>assets/front/js/jquery.cycle2.carousel.js"></script>
