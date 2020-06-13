5fe7548dc4ea7cb4d1285cfa4b74488b113bd08c
zeppyshiddiq <zeppyshiddiq@gmail.com>
2020-06-13 20:14:00 +0700

    Penyesuaian Perubahan Config ke Menu Admin Web -> Pengaturan

--- commons/source_js.php
+++ commons/source_js.php
@@ -1,7 +1,10 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <script src="<?= base_url("$this->theme_folder/$this->theme/assets/js/vendor.min.js") ?>"></script>
-<script src="<?= base_url('assets/js/highcharts/highcharts.js')?>"></script>
+<script src="<?= base_url()?>assets/js/highcharts/highcharts.js"></script>
+<script src="<?= base_url()?>assets/js/highcharts/highcharts-3d.js"></script>
+<script src="<?= base_url()?>assets/js/highcharts/exporting.js"></script>
+<script src="<?= base_url()?>assets/js/highcharts/highcharts-more.js"></script>
 <script src="<?= base_url("assets/front/js/jquery.cycle2.min.js")?>"></script>
 <script src="<?= base_url("assets/front/js/jquery.cycle2.carousel.js")?>"></script>
 <script src="<?= base_url("assets/js/leaflet.js")?>"></script>
